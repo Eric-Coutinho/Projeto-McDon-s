d676904f053cda4cf87f98229165aa32f9e07007
eric.coutinhop@hotmail.com <eric.coutinhop@hotmail.com>
2023-11-23 16:55:26 -0300

    Product page almost done

--- McDons-app/src/app/produto/produto.component.ts
+++ McDons-app/src/app/produto/produto.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-produto',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule],
   templateUrl: './produto.component.html',
   styleUrl: './produto.component.css'
 })
