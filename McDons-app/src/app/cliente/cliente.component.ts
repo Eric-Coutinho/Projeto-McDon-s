5b81efe3999c81cf62558334c50c5e8bb158c764
eric.coutinhop@hotmail.com <eric.coutinhop@hotmail.com>
2023-11-23 16:11:40 -0300

    Navigation on Menu functional

--- McDons-app/src/app/cliente/cliente.component.ts
+++ McDons-app/src/app/cliente/cliente.component.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, ViewportScroller } from '@angular/common';
 import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-cliente',
   standalone: true,
-  imports: [CommonModule, MatIconModule],
+  imports: [CommonModule, MatIconModule, ],
   templateUrl: './cliente.component.html',
   styleUrl: './cliente.component.css'
 })
 export class ClienteComponent {
+  constructor(private viewportScroller: ViewportScroller) {}
+  
   public navigateToSection(section: string){
-    window.location.hash = '';
-    window.location.hash = section;
+    // window.location.hash = '';
+    // window.location.hash = section;
+    this.viewportScroller.scrollToAnchor(section);
   }
 }
