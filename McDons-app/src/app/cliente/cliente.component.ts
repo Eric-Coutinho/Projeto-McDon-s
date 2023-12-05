4039225148b4aa6b799b99673eb20cbfddc85384
Eric-Coutinho <eric.coutinhop@hotmail.com>
2023-12-05 16:14:54 -0300

    Mostrar produtos funcionando em todas as paginas que precisa

--- McDons-app/src/app/cliente/cliente.component.ts
+++ McDons-app/src/app/cliente/cliente.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule, ViewportScroller } from '@angular/common';
 import {MatIconModule} from '@angular/material/icon';
+import { ProdutoService } from '../service/produto-service';
 
 @Component({
   selector: 'app-cliente',
@@ -10,9 +11,41 @@ import {MatIconModule} from '@angular/material/icon';
   styleUrl: './cliente.component.css'
 })
 export class ClienteComponent {
-  constructor(private viewportScroller: ViewportScroller) {}
+  constructor(private products: ProdutoService, private viewportScroller: ViewportScroller) { }
   
+  list: any = [];
+  hamburguerList: any = [];
+  bebidaList: any = [];
+  acompanhamentoList: any = [];
+  sobremesaList: any = [];
+  
+  goToInfo(){
+    console.log("oiii")
+  }
+
   public navigateToSection(section: string){
     this.viewportScroller.scrollToAnchor(section);
   }
+
+  ngOnInit() {
+    this.products.getAll().subscribe(
+      (data: any) => {
+        this.list = data;
+        console.log("produtos:", this.list);
+  
+        this.hamburguerList = this.list.filter((produto: any) => produto.tipo === 'Hamburguer');
+        this.bebidaList = this.list.filter((produto: any) => produto.tipo === 'Bebida');
+        this.acompanhamentoList = this.list.filter((produto: any) => produto.tipo === 'Acompanhamento');
+        this.sobremesaList = this.list.filter((produto: any) => produto.tipo === 'Sobremesa');
+      },
+      (error: any) => {
+        console.error('Erro ao obter produtos:', error);
+      }
+    );
+  }
+
+  logTipo(tipo: string) {
+  console.log('Tipo:', tipo);
+  return tipo;
+}
 }
--- McDons-app/src/app/totem/totem.component.ts
+++ McDons-app/src/app/totem/totem.component.ts
@@ -4,6 +4,7 @@ import { CommonModule, ViewportScroller } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 
 import { Router } from '@angular/router';
+import { ProdutoService } from '../service/produto-service';
 
 @Component({
   selector: 'app-totem',
@@ -14,7 +15,39 @@ import { Router } from '@angular/router';
 })
 
 export class TotemComponent {
-  constructor(private viewportScroller: ViewportScroller, private router: Router) {}
+  constructor(private viewportScroller: ViewportScroller, private router: Router, private products: ProdutoService) {}
+  
+  list: any = [];
+  hamburguerList: any = [];
+  bebidaList: any = [];
+  acompanhamentoList: any = [];
+  sobremesaList: any = [];
+  
+  goToInfo(){
+    console.log("oiii")
+  }
+
+  ngOnInit() {
+    this.products.getAll().subscribe(
+      (data: any) => {
+        this.list = data;
+        console.log("produtos:", this.list);
+  
+        this.hamburguerList = this.list.filter((produto: any) => produto.tipo === 'Hamburguer');
+        this.bebidaList = this.list.filter((produto: any) => produto.tipo === 'Bebida');
+        this.acompanhamentoList = this.list.filter((produto: any) => produto.tipo === 'Acompanhamento');
+        this.sobremesaList = this.list.filter((produto: any) => produto.tipo === 'Sobremesa');
+      },
+      (error: any) => {
+        console.error('Erro ao obter produtos:', error);
+      }
+    );
+  }
+
+  logTipo(tipo: string) {
+  console.log('Tipo:', tipo);
+  return tipo;
+}
   
   public navigateToSection(section: string){
     this.viewportScroller.scrollToAnchor(section);
